44adda3ba10c8b6a5723f2f009c4e46202027c21
zammmsara <sara.zambrana@gmail.com>
2026-06-10 08:49:11 -0600

    style: mejorar consistencia visual entre formulario y diálogos

--- mobile/nightbiteApp/app/src/main/java/ni/edu/uam/nightbiteapp/ui/components/NightMessageDialog.kt
+++ mobile/nightbiteApp/app/src/main/java/ni/edu/uam/nightbiteapp/ui/components/NightMessageDialog.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import ni.edu.uam.nightbiteapp.ui.theme.CheeseYellow
 import ni.edu.uam.nightbiteapp.ui.theme.LavenderGray
 import ni.edu.uam.nightbiteapp.ui.theme.NightSurface
@@ -57,7 +58,7 @@ fun NightMessageDialog(
     Dialog(
         onDismissRequest = {
             onDismiss?.invoke()
-        }
+        },
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
